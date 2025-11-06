4116cc0fb4713ce25bcbf0b5e557211f67275103
pedrl1 <pedroleite3010@hotmail.com>
2025-11-06 17:44:35 -0300

    GUI folder created

---
+++ GUI/output/GUI.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+const char g_szClassName[] = "ClasseDaJanela";
+
+int WINAPI Winmain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG msg;
+
+    //size of 
+}
