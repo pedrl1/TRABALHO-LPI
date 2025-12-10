ef9cbabf884a627126cff58dbb9e00d472813daf
Thiago-Galvao17 <thiagoprogamer952@gmail.com>
2025-12-10 04:25:11 -0300

    Update parser.h versão com extraiDadosLocalizacao

--- src/parser.h
+++ src/parser.h
@@ -1,24 +1,29 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct
+typedef struct DadosTxt
 {
     char endereco [256];
-    char cidade [32];
+    char numero [32];   // --nova variavel
+    char bairro [32];   // --nova variavel
+    char cidade [32];   // --nova variavel
     char regiao [32];
     char conf_frente [256];
     char conf_fundo [256];
     char conf_latEsq [256];
     char conf_latDir [256];
-    char coords [16];
-    char descricao [512];
+    char coords [64];
     char area_tot [32];
     char area_usada [32];
     char conserv_state [32];
     char valoracao [256];
-    char data_valoracao [16];
+    char data_valoracao [32];
     char CPNo [32];
-} dado_excel;
+} Dadostxt;
+
+typedef Dadostxt dado_excel;
+
+void extraiDadosLocalizacao(char *p_linha, dado_excel *dado_l); //--Função que extrai endereço, número, bairro e cidade.
 
 void parser_txt(const char *local, dado_excel *out);
 
